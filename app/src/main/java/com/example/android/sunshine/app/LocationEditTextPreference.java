adde087e4f8952030b5dd073ac0f0f7653c52feb
vuonght <vuonght@users.noreply.github.com>
2015-10-08 03:46:31 -0700

    [IPEC-20] Implemented validation logic for the location preference to only enable the `OK` button if the minimum length of characters required is satisfied.

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -1,10 +1,17 @@
 package com.example.android.sunshine.app;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by silare on 10/8/15.
@@ -35,5 +42,38 @@ public class LocationEditTextPreference extends EditTextPreference
         {
             typedArray.recycle();
         }
+    }
+
+    @Override
+    protected void showDialog(Bundle state)
+    {
+        super.showDialog(state);
+        EditText editText = getEditText();
+        editText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog)
+                {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    positiveButton.setEnabled(s.length() >= minLength);
+                }
+            }
+        });
     }
 }
